db8d62b42907ecb6e5efe628139b4c471f23a17b
Jonathan Bijl <sannoayo@gmail.com>
2023-07-08 10:58:10 -0400

    react form

--- src/App.js
+++ src/App.js
@@ -1,7 +1,52 @@
-import "./App.css";
- 
+import React, { useState } from 'react';
+import './App.css';
+
 function App() {
-  return <div className="App"></div>;
+  const [taskList, setTaskList] = useState([]);
+  const [formData, setFormData] = useState({
+    title: "",
+    task: "",
+    isComplete: ""
+  });
+
+  const handleFormSubmit = (e) => {
+    e.preventDefault();
+    
+  };
+
+  const handleChange = (e) => {
+    const { name, value, type, checked } = e.target;
+    const newValue = type === "checkbox" ? checked : value;
+    setFormData((prevData) => ({
+      ...prevData,
+      [name]: newValue
+    }));
+  };
+
+  return (
+    <div className="App">
+      <h1>Iron Task List</h1>
+      <br />
+      <form onSubmit={handleFormSubmit}>
+        <label>
+          Title: 
+          <input type='text' name="title" value={formData.title} onChange={handleChange} />
+        </label>
+        <br />
+        <label>
+          Task: 
+          <input type='text' name="task" value={formData.task} onChange={handleChange} />
+        </label>
+        <br />
+        <label>
+          Is Complete: 
+          <input type='checkbox' name="isComplete" checked={formData.isComplete} onChange={handleChange} />
+        </label>
+        <br />
+        <button type="submit">Send</button>
+      </form>
+    </div>
+  );
 }
- 
+
 export default App;
